5f13a23d5d316ffb3ad0d00e87471cf8e0750e36
Juan Olaya <juandavid.olaya@globant.com>
2024-02-15 16:00:15 -0500

    types of variables and how I can know what type is

---
+++ typesData.py
@@ -0,0 +1,54 @@
+''' Primitives '''
+
+# string
+name = "Juan Olaya"
+name1 = 'Juan Olaya'
+
+print(type(name))
+print(type(name1))
+
+# integer
+age = 20
+
+print(type(age))
+
+# float
+height = 1.72
+
+print(type(height))
+
+# boolean
+single = True
+
+print(type(single))
+
+# complex numbers
+complexNumber = 2 + 4j
+
+print(type(complexNumber)) # the complex numbers have a real part and an imaginary part
+
+''' No primitive '''
+
+# list
+names = ["Jeidy", "Juan", "Karen", "Carlos"]
+
+print(type(names))
+
+# dictionario
+skills = {
+    "Nicolas": 3,
+    "Marcos": 5,
+    "Natalia": 2    
+}
+
+print(type(skills))
+
+# tuple
+kingSkills = ("programming", "english", "soft skill", "data driven")
+
+print(type(kingSkills))
+
+# set
+numbers = {3, 2, 5, 3, 9}
+
+print(type(numbers))
